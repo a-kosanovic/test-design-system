1023c2dcacb9675ec335670d3b55dc5676c61fb3
Charlie Draper <charles.draper@brandwatch.com>
2022-09-09 13:33:19 +0100

    chore: add storybook a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,9 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../packages/**/*.stories.@(js|ts|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components"
-}
+  framework: '@storybook/web-components',
+};
