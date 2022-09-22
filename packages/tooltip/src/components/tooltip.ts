c4fc10fad7bbf83a7cd3d6ea01399675d2f49c9c
Aleksandra Kosanovic <aleksandra.kosanovic@brandwatch.com>
2022-09-22 23:52:15 +0200

    move css to styles file

--- packages/tooltip/src/components/tooltip.ts
+++ packages/tooltip/src/components/tooltip.ts
@@ -1,65 +1,37 @@
-import {html, LitElement, css} from 'lit';
+import {html, LitElement} from 'lit';
 import {customElement} from 'lit/decorators.js';
+import styles from './tooltip.styles';
 
 @customElement('ds-tooltip')
 export class Tooltip extends LitElement {
-  // Todo: move styles to separate file
-  static styles = css`
-    :host {
-      --text-color: #404040;
-      --popup-box-shadow: 0 0 0 0.0625rem hsla(0, 0%, 25%, 0.2),
-        0 0.25rem 0.5625rem 0 hsla(0, 0%, 25%, 0.14),
-        0 0.0625rem 0.125rem 0.03125rem hsla(0, 0%, 25%, 0.15);
-      --background-color: hsl(0, 0%, 100%);
-      --border-radius: 0.1875rem;
-      --box-shadow: 0 0 0 0.0625rem hsla(0, 0%, 25%, 0.2),
-        0 0.25rem 0.5625rem 0 hsla(0, 0%, 25%, 0.14),
-        0 0.0625rem 0.125rem 0.03125rem hsla(0, 0%, 25%, 0.15);
+  static styles = [...styles];
 
-      // Arrow Styling
-      --arrow-width: 0.75rem;
-      --arrow-offset: calc(var(--arrow-width) / -2);
-      --tip-size: 0.75rem;
+  static properties = {
+    isVisible: {},
+  };
 
-      position: absolute;
-      inset: 0px auto auto 0px;
-      margin: 0px;
-      transform: translate(30px, 59px);
+  constructor() {
+    super();
+    this.isVisible = false;
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('isVisible')) {
+      this.isVisible.toString() === 'true'
+        ? this.showTooltip()
+        : this.hideTooltip();
     }
+  }
 
-    ::slotted(*) {
-      position: absolute;
-      border-radius: var(--border-radius, 0.1875rem);
-      box-shadow: var(--box-shadow);
-      background-color: var(--background-color, white);
-      color: var(--text-color);
-      display: block;
-      overflow-y: auto;
-      scroll-behavior: smooth;
-    }
+  showTooltip() {
+    this.style.visibility = '';
+    this.style.cssText = '';
+  }
 
-    .arrow {
-      z-index: -1;
-      position: absolute;
-      top: calc(var(--tip-size) * -0.5);
-      left: 0px;
-      transform: translate(17px, 0px);
-      display: block;
-      width: var(--arrow-width);
-      height: var(--arrow-width);
-    }
-
-    .arrow::before {
-      content: '';
-      position: absolute;
-      width: var(--tip-size);
-      height: var(--tip-size);
-      transform: rotate(45deg);
-      box-shadow: var(--popup-box-shadow);
-      background-color: var(--background-color);
-      pointer-events: none;
-    }
-  `;
+  hideTooltip() {
+    this.style.visibility = 'hidden';
+    this.style.opacity = '0';
+  }
 
   render() {
     return html`
--- packages/tooltip/stories/tooltip.stories.ts
+++ packages/tooltip/stories/tooltip.stories.ts
@@ -4,15 +4,18 @@ import '../src/index';
 const config = {
   title: 'Components/Tooltip',
   component: 'ds-tooltip',
+  argTypes: {
+    Visible: {control: 'boolean', default: true},
+  },
 };
 
 export default config;
 
-function Template() {
+function Template(args): any {
   return html`
     <ds-button></ds-button>
 
-    <ds-tooltip>
+    <ds-tooltip isvisible=${args.Visible}>
       <div>Hello from Tooltip</div>
     </ds-tooltip>
   `;
